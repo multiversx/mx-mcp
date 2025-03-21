9a92c54aca2204edb6af3bdbee89f2668c1dd90a
Alexandru Popenta <alexandru.p9913@gmail.com>
2025-03-21 12:09:22 +0200

    add more actions

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
-import { homedir } from "os"; // Import `homedir` to expand `~`
+import { homedir } from "os";
 import { resolve } from "path";
 
-export const MULTIVERSX_DIR = resolve(homedir(), ".multiversx"); // Correctly expand home directory
+export const MULTIVERSX_DIR = resolve(homedir(), ".multiversx");
 export const MULTIVERSX_WALLET_NAME = "wallet.pem";
 export const EGLD_NUM_DECIMALS = 18;
--- src/index.ts
+++ src/index.ts
@@ -380,6 +380,316 @@ server.tool(
   }
 );
 
+server.tool(
+  "issue-fungible-token",
+  "Create a transaction to issue a fungible token and send it. Will issue the token with the specified arguments. All the properties will be set to true.",
+  {
+    tokenName: z.string().describe("The token name."),
+    tokenTicker: z.string().describe("The token ticker."),
+    initialSupply: z
+      .string()
+      .describe("The initial supply that will be minted."),
+    numDecimals: z
+      .string()
+      .describe("The number of decimals the token will have."),
+  },
+  async ({ tokenName, tokenTicker, initialSupply, numDecimals }) => {
+    const pem = loadPemWalletFromEnv();
+    const account = new Account(pem.secretKey);
+
+    const network = process.env.MVX_NETWORK;
+    const entrypoint = getEntrypoint(network);
+
+    account.nonce = await entrypoint.recallAccountNonce(account.address);
+
+    const controller = entrypoint.createTokenManagementController();
+    const transaction = await controller.createTransactionForIssuingFungible(
+      account,
+      account.nonce,
+      {
+        tokenName: tokenName,
+        tokenTicker: tokenTicker,
+        initialSupply: BigInt(initialSupply),
+        numDecimals: BigInt(numDecimals),
+        canFreeze: true,
+        canWipe: true,
+        canPause: true,
+        canChangeOwner: true,
+        canUpgrade: true,
+        canAddSpecialRoles: true,
+      }
+    );
+
+    const hash = await entrypoint.sendTransaction(transaction);
+    const explorer = getExplorerUrl(network);
+    return {
+      content: [
+        {
+          type: "text",
+          text: `The transaction has been sent. Check out the transaction here: ${explorer}/transactions/${hash}`,
+        },
+      ],
+    };
+  }
+);
+
+server.tool(
+  "issue-semi-fungible-collection",
+  "Create a transaction to issue a semi-fungible collection (SFT) and send it. Will issue the collection with the specified arguments. All the properties will be set to true.",
+  {
+    tokenName: z.string().describe("The token name."),
+    tokenTicker: z.string().describe("The token ticker."),
+  },
+  async ({ tokenName, tokenTicker }) => {
+    const pem = loadPemWalletFromEnv();
+    const account = new Account(pem.secretKey);
+
+    const network = process.env.MVX_NETWORK;
+    const entrypoint = getEntrypoint(network);
+
+    account.nonce = await entrypoint.recallAccountNonce(account.address);
+
+    const controller = entrypoint.createTokenManagementController();
+    let transaction = await controller.createTransactionForIssuingSemiFungible(
+      account,
+      account.getNonceThenIncrement(),
+      {
+        tokenName: tokenName,
+        tokenTicker: tokenTicker,
+        canFreeze: true,
+        canWipe: true,
+        canPause: true,
+        canChangeOwner: true,
+        canUpgrade: true,
+        canAddSpecialRoles: true,
+        canTransferNFTCreateRole: true,
+      }
+    );
+
+    const issueCollectionHash = await entrypoint.sendTransaction(transaction);
+    const outcome = await controller.awaitCompletedIssueSemiFungible(
+      issueCollectionHash
+    );
+
+    const token = outcome[0].tokenIdentifier;
+    transaction =
+      await controller.createTransactionForSettingSpecialRoleOnSemiFungibleToken(
+        account,
+        account.nonce,
+        {
+          user: account.address,
+          tokenIdentifier: token,
+          addRoleNFTCreate: true,
+          addRoleNFTBurn: true,
+          addRoleESDTTransferRole: true,
+          addRoleNFTAddQuantity: true,
+        }
+      );
+    const setRolesHash = await entrypoint.sendTransaction(transaction);
+
+    const explorer = getExplorerUrl(network);
+    return {
+      content: [
+        {
+          type: "text",
+          text: `The transaction has been sent. Check out the transaction here: ${explorer}/transactions/${issueCollectionHash}. A transaction to set roles has also been sent: ${explorer}/transactions/${setRolesHash}`,
+        },
+      ],
+    };
+  }
+);
+
+server.tool(
+  "issue-nft-collection",
+  "Create a transaction to issue a non-fungible token collection (NFT) and send it. Will issue the collection with the specified arguments. All the properties will be set to true.",
+  {
+    tokenName: z.string().describe("The token name."),
+    tokenTicker: z.string().describe("The token ticker."),
+  },
+  async ({ tokenName, tokenTicker }) => {
+    const pem = loadPemWalletFromEnv();
+    const account = new Account(pem.secretKey);
+
+    const network = process.env.MVX_NETWORK;
+    const entrypoint = getEntrypoint(network);
+
+    account.nonce = await entrypoint.recallAccountNonce(account.address);
+
+    const controller = entrypoint.createTokenManagementController();
+    let transaction = await controller.createTransactionForIssuingNonFungible(
+      account,
+      account.getNonceThenIncrement(),
+      {
+        tokenName: tokenName,
+        tokenTicker: tokenTicker,
+        canFreeze: true,
+        canWipe: true,
+        canPause: true,
+        canChangeOwner: true,
+        canUpgrade: true,
+        canAddSpecialRoles: true,
+        canTransferNFTCreateRole: true,
+      }
+    );
+
+    const issueCollectionHash = await entrypoint.sendTransaction(transaction);
+    const outcome = await controller.awaitCompletedIssueNonFungible(
+      issueCollectionHash
+    );
+
+    const token = outcome[0].tokenIdentifier;
+    transaction =
+      await controller.createTransactionForSettingSpecialRoleOnNonFungibleToken(
+        account,
+        account.nonce,
+        {
+          user: account.address,
+          tokenIdentifier: token,
+          addRoleNFTCreate: true,
+          addRoleNFTBurn: true,
+          addRoleESDTTransferRole: true,
+          addRoleNFTAddURI: true,
+          addRoleNFTUpdateAttributes: true,
+          addRoleESDTModifyCreator: true,
+          addRoleESDTModifyRoyalties: true,
+          addRoleESDTSetNewURI: true,
+          addRoleNFTRecreate: true,
+        }
+      );
+    const setRolesHash = await entrypoint.sendTransaction(transaction);
+
+    const explorer = getExplorerUrl(network);
+    return {
+      content: [
+        {
+          type: "text",
+          text: `The transaction has been sent. Check out the transaction here: ${explorer}/transactions/${issueCollectionHash}. A transaction to set roles has also been sent: ${explorer}/transactions/${setRolesHash}`,
+        },
+      ],
+    };
+  }
+);
+
+server.tool(
+  "issue-meta-esdt-collection",
+  "Create a transaction to issue a MetaESDT token collection (MESDT) and send it. Will issue the collection with the specified arguments. All the properties will be set to true.",
+  {
+    tokenName: z.string().describe("The token name."),
+    tokenTicker: z.string().describe("The token ticker."),
+    numDecimals: z.string().describe("The number of decimals."),
+  },
+  async ({ tokenName, tokenTicker, numDecimals }) => {
+    const pem = loadPemWalletFromEnv();
+    const account = new Account(pem.secretKey);
+
+    const network = process.env.MVX_NETWORK;
+    const entrypoint = getEntrypoint(network);
+
+    account.nonce = await entrypoint.recallAccountNonce(account.address);
+
+    const controller = entrypoint.createTokenManagementController();
+    let transaction = await controller.createTransactionForRegisteringMetaEsdt(
+      account,
+      account.getNonceThenIncrement(),
+      {
+        tokenName: tokenName,
+        tokenTicker: tokenTicker,
+        numDecimals: BigInt(numDecimals),
+        canFreeze: true,
+        canWipe: true,
+        canPause: true,
+        canChangeOwner: true,
+        canUpgrade: true,
+        canAddSpecialRoles: true,
+        canTransferNFTCreateRole: true,
+      }
+    );
+
+    const issueCollectionHash = await entrypoint.sendTransaction(transaction);
+    const outcome = await controller.awaitCompletedRegisterMetaEsdt(
+      issueCollectionHash
+    );
+
+    const token = outcome[0].tokenIdentifier;
+    transaction =
+      await controller.createTransactionForSettingSpecialRoleOnMetaESDT(
+        account,
+        account.nonce,
+        {
+          user: account.address,
+          tokenIdentifier: token,
+          addRoleNFTCreate: true,
+          addRoleNFTBurn: true,
+          addRoleESDTTransferRole: true,
+          addRoleNFTAddQuantity: true,
+        }
+      );
+    const setRolesHash = await entrypoint.sendTransaction(transaction);
+
+    const explorer = getExplorerUrl(network);
+    return {
+      content: [
+        {
+          type: "text",
+          text: `The transaction has been sent. Check out the transaction here: ${explorer}/transactions/${issueCollectionHash}. A transaction to set roles has also been sent: ${explorer}/transactions/${setRolesHash}`,
+        },
+      ],
+    };
+  }
+);
+
+server.tool(
+  "issue-semi-fungible-or-nft-or-meta-esdt-token",
+  "Create a transaction to issue a semi-fungible token (SFT), or a non-fungible token (NFT), or a MetaESDT token for a collection and send it. Will issue the token with the specified arguments.",
+  {
+    tokenIdentifier: z.string().describe("The identifier of the collection."),
+    initialQuantity: z
+      .string()
+      .optional()
+      .describe(
+        "The initial quantity that will be minted. If not provided, defaults to 1."
+      ),
+    name: z.string().describe("The name of the token."),
+    royalties: z.string().optional().describe("The royalties you'll receive."),
+  },
+  async ({ tokenIdentifier, initialQuantity, name, royalties }) => {
+    const pem = loadPemWalletFromEnv();
+    const account = new Account(pem.secretKey);
+
+    const network = process.env.MVX_NETWORK;
+    const entrypoint = getEntrypoint(network);
+
+    account.nonce = await entrypoint.recallAccountNonce(account.address);
+
+    const controller = entrypoint.createTokenManagementController();
+    let transaction = await controller.createTransactionForCreatingNft(
+      account,
+      account.getNonceThenIncrement(),
+      {
+        tokenIdentifier: tokenIdentifier,
+        initialQuantity: initialQuantity ? BigInt(initialQuantity) : 1n,
+        name: name,
+        royalties: royalties ? Number(royalties) * 100 : 0,
+        hash: "",
+        attributes: new Uint8Array(),
+        uris: [],
+      }
+    );
+
+    const hash = await entrypoint.sendTransaction(transaction);
+
+    const explorer = getExplorerUrl(network);
+    return {
+      content: [
+        {
+          type: "text",
+          text: `The transaction has been sent. Check out the transaction here: ${explorer}/transactions/${hash}.`,
+        },
+      ],
+    };
+  }
+);
+
 async function main() {
   const transport = new StdioServerTransport();
   await server.connect(transport);
